a0e6bedc8774f374ecf902f59f41cca2fc9cac5d
Mikepag <p14pagk@ionio.gr>
2018-03-13 12:38:01 +0200

    Update matrix1.c

--- matrix1.c
+++ matrix1.c
@@ -2,53 +2,93 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-// compile like:  gcc -Wall -O2 -DNROWS=10000 matrix1.c -o matrix1
-
+// compile like:  gcc -Wall -O2 -DNROWS=1000 matrix1.c -o matrix1
 
 #define NCOLS 100
 
 void get_walltime(double *wct) {
-  struct timeval tp;
-  gettimeofday(&tp,NULL);
-  *wct = (double)(tp.tv_sec+tp.tv_usec/1000000.0);
+	struct timeval tp;
+	gettimeofday(&tp,NULL);
+	*wct = (double)(tp.tv_sec+tp.tv_usec/1000000.0);
 }
 
 
-int main() {
-double *table;
-double ts,te;
+int main()
+{
+	int i, j;	//counters
+	double *table;
+	double ts,te;
+	int error = 0;	//0=FALSE, 1=TRUE
+	double elapsed_time;	//used to calculate the time elapsed while accessing the matrix
+	
+	FILE *table_fptr;	//Pointer to the .csv file "TableValues.csv" where the values of matrix "table" are saved
+	table_fptr = fopen("TableValues.csv", "w");
+	
+	FILE *time_fptr;	//Pointer to the .csv file "ElapsedTime.csv" where the time elapsed while accessing the matrix is saved
+	time_fptr = fopen("ElapsedTime.csv", "w");
+	
+	table = (double *)malloc(NROWS*NCOLS*sizeof(double)); 
+	if (table==NULL) {
+		printf("alloc error!\n");
+		exit(1);
+	}
 
 
-  table = (double *)malloc(NROWS*NCOLS*sizeof(double)); 
-  if (table==NULL) {
-    printf("alloc error!\n");
-    exit(1);
-  }
 
-  // warmup
+	// warmup - Matrix Initialization
 
-  // ...your code here...
+	for(i=0; i<NROWS*NCOLS; i++)
+	{
+		table[i] = 2;
+		
+	}
+	
 
-  // get starting time (double, seconds) 
-  get_walltime(&ts);
+//____________________________________________________________________________
+
+  	// get starting time (double, seconds) 
+  	get_walltime(&ts);
   
-  // workload
+  	// workload
 
-  // ...your code here...
+	for(i=0; i<NCOLS; i++)
+	{
+		for(j=0; j<NROWS; j++)
+		{
+			table[j+NROWS*i] = table[j+NROWS*i] + 3;	// prospelash ANA GRAMMH
+			fprintf(table_fptr, "%lf\n", table[i+NCOLS*j]);	//print in .csv
+		}
+	}
+	
+	// get ending time
+	get_walltime(&te);
+//____________________________________________________________________________
+	
+	// check results
 
-  // get ending time
-  get_walltime(&te);
+	for(i=0; i<NROWS*NCOLS; i++)	//Each table cell must have the value 5.	
+	{
+		
+		if(table[i] != 5)	// If not, something must have gone wrong while accessing the table
+		{
+			error = 1;
+		}
+	}
+	if(error != 0)
+	{
+		printf("Something went wrong while accessing the table");
+	}
+	
+	
+	// print time elapsed and/or Maccesses/sec
+	
+	elapsed_time = te-ts;	//elapsed_time = ending_time - starting_time
+	fprintf(time_fptr, "%lf", elapsed_time);
+	printf("\n\nTimeElapsed: %lf\n", elapsed_time);
+	
+	free(table);
+	fclose(table_fptr);
+	fclose(time_fptr);
 
-  // check results
-  
-  // ...your code here...
-
-  // print time elapsed and/or Maccesses/sec
-  
-  // ...your code here...  
-  
-  free(table);
-
-  return 0;
+	return 0;
 }
-
